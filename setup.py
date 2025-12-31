f6589640b8f1b487872da261fc200f03e7f50782
elJulioDev <alexisdevelopgz@gmail.com>
2025-12-31 13:56:35 -0300

    Update setup.py to version 0.2.0 and enhance dependencies

--- setup.py
+++ setup.py
@@ -9,20 +9,22 @@ if os.path.exists("README.md"):
 
 setup(
     name="NeuralNetwork",
-    version="0.1.0",
+    version="0.2.0", 
     packages=find_packages(),
     install_requires=[
-        "numpy>=1.20.0",
-        "python-dotenv"
+        "numpy>=1.21.0", 
+        "python-dotenv",
+        "matplotlib>=3.5.0"
     ],
     author="elJulioDev",
-    description="Una librería de Deep Learning ligera construida desde cero",
-    long_description=long_description, # Usamos la variable segura
+    description="Una librería de Deep Learning vectorizada y ligera desde cero",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires='>=3.8',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
